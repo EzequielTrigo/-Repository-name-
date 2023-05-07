2274e26a6d33c5cb5471df31657e5bf43c328849
totoquik <tomas.andujar08@gmail.com>
2023-05-07 15:06:45 -0300

    nombres integrantes, segun planilla excel.

--- iap1-tp.hs
+++ iap1-tp.hs
@@ -1,10 +1,10 @@
 -- Completar con los datos del grupo
 --
 -- Nombre de Grupo: 404_Name_Not_Found
--- Integrante 1: Nombre Apellido, email, LU
--- Integrante 2: Nombre Apellido, email, LU
--- Integrante 3: Nombre Apellido, email, LU
--- Integrante 4: Nombre Apellido, email, LU
+-- Integrante 1: Integrante 1: Ezequiel Trigo , ezequiel0trigo@gmail.com, 603/22
+-- Integrante 2: Joaquin Piaggio, piaggiojoaquinpiaggio@gmail.com, 700/22
+-- Integrante 3: Tomas Agustin Andujar Ferreira, tomas.andujar02@gmail.com, 823/22
+-- Integrante 4: Francisco Nistal, franistal3@gmail.com, 905/22
 
 type Usuario = (Integer, String) -- (id, nombre)
 type Relacion = (Usuario, Usuario) -- usuarios que se relacionan
